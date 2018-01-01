861cab825e5e21a85abbc236c7ed691302684054
Mohammad Khan <sparrowangels@gmail.com>
2018-01-01 06:19:09 +0000

    REDO for interview : method: take_a_number()

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,29 +4,37 @@ katz_deli = []
 # line method ******************************************************************************
 # shows everyone their current place in the line.
 #   If there is nobody in line, it should say "The line is currently empty."
-# accecpt one argument
-#   katz_queue : which an array which holds customers
-def line(katz_queue=katz_deli)
+# arguments : one
+#   katz_deli : which an array which holds customers
+
+def line(katz_deli=katz_deli)
   output = "The line is currently"            #this string can be used with both possibilities of "if"
-  if katz_queue.length == 0
+  if katz_deli.length == 0
     output += " empty."
   else
     output += ":"
-    for i in 0..katz_queue.length-1 do
-      output += " #{i+1}. #{katz_queue[i]}"
+    for i in 0..katz_deli.length-1 do
+      output += " #{i+1}. #{katz_deli[i]}"
     end #for
   end #if
   puts output
 end #line
 
 # take_a_number method *********************************************************************
-# accept two arguments,
+# method that a new customer will use when entering the deli.
+# arguments : two
 #   the array for the current line of people (katz_deli),
 #   and a string containing the name of the person wishing to join the line.
-# return
-#   the person's name along with their position in line.
-# method that a new customer will use when entering the deli.
+# puts with the person's name along with their position in line.
+
+
+def take_a_number(katz_deli=katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end #take_a_number
 
 # now_serving method ***********************************************************************
 # should call out (i.e. puts) the next person in line and then remove them from the front.
 # If there is nobody in line, it should call out (puts) that "There is nobody waiting to be served!".
+# arguments : one
+#   katz_deli : which an array which holds customers
